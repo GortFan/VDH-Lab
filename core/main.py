ef6ef67cbee3f38f00ca893e12afde55f6a7a44b
GortFan <k.trichiliocosm@gmail.com>
2025-11-11 18:16:58 -0500

    make main not ugly and dumb

--- core/main.py
+++ core/main.py
@@ -11,28 +11,28 @@ network = project.network
 
 network = topography.segment_electrode(network=network)
 network = topography.create_ff_pores(network=network)
-network = topography.add_ff_to_electrode_throats(network=network)
-network = topography.add_ff_to_ff_throats(network=network)
+network = topography.connect_ff_pores_eachother(network=network)
+network = topography.connect_electrode_to_ff_pores(network=network)
 network = topography.define_membrane(network=network)
 network = topography.define_ff_boundaries(network=network)
 
 ws.save_project(project, filename='segmented_freudenberg.pnm')
-op.io.project_to_vtk(project, filename="yerrrrrr")
+op.io.project_to_vtk(project, filename="test")
 
 # ==================== CREATE PHASES ====================
 anolyte, catholyte = simulation.create_phases(network, params)
 
 # ==================== CALCULATE INLET FLOW RATE ====================
-v_in_c = params['catholyte_inlet_velocity']
+inlet_velocity = params['catholyte_inlet_velocity'] # anolyte inlet velocity is identical
 inlet_pores = network.pores('inlet')
 cross_area = np.pi / 4 * network['pore.diameter']**2
 A_in_c = np.sum(cross_area[inlet_pores])
-Q_in_c = v_in_c * A_in_c
+Q_in_c = inlet_velocity * A_in_c
 
 # ==================== SETUP STOKES FLOW ====================
 sf_catholyte = simulation.setup_stokes_flow(network, catholyte, Q_inlet=Q_in_c, P_outlet=0.0)
 
 # ==================== EXPORT ====================
-project.update({'catholyte': catholyte})
+project.append(catholyte)
 ws.save_project(project, filename='stokes_test.pnm')
 op.io.project_to_vtk(project, filename="stokes_test")
